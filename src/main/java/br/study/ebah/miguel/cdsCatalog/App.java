62cc64c5925a3a42ec1ac29bd69a58949586867f
Miguel Mendes Ruiz <miguel@ebah.com.br>
2012-07-05 18:32:19 -0300

    moving usage example to App

--- src/main/java/br/study/ebah/miguel/cdsCatalog/App.java
+++ src/main/java/br/study/ebah/miguel/cdsCatalog/App.java
@@ -1,13 +1,26 @@
 package br.study.ebah.miguel.cdsCatalog;
 
+import java.util.Date;
+
+import br.study.ebah.miguel.cdsCatalog.elements.Disc;
+import br.study.ebah.miguel.cdsCatalog.inMemory.InMemoryArtistRW;
+import br.study.ebah.miguel.cdsCatalog.inMemory.InMemoryDisc;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			InMemoryArtistRW artist = new InMemoryArtistRW("Eu", new Date());
+			Disc disc = new InMemoryDisc("", new Date());
+			System.out.println("Adding disc: " + disc);
+			artist.asAddable(Disc.class).add(disc);
+			System.out.println("Artist's known discs: "
+					+ artist.getKnownDiscs());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
